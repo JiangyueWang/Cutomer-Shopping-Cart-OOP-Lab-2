3a5ce5aa364eccee32ce2d755c66985446749e8e
Jiang <jiangyue0380@gmail.com>
2022-07-18 13:59:49 +1000

    initiate and update product.py

---
+++ product.py
@@ -0,0 +1,6 @@
+class Product:
+
+    def __init__(self, product_name, price, category):
+        self.product_name = product_name
+        self.product_price = price
+        self.product_category = category
